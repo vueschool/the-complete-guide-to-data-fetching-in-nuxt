36db40e48797b3705d3da2d5334ecbeeb1af6e74
Daniel Kelly <me@danielkelly.io>
2024-10-15 15:27:14 -0500

    1-intro-to-data-fetching-in-nuxt

--- app.vue
+++ app.vue
@@ -1,6 +1,20 @@
+<script setup lang="ts">
+const { data: pokemons } = await useFetch(
+  "https://dummyapi.online/api/pokemon"
+);
+</script>
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <ul>
+    <li v-for="pokemon in pokemons" :key="pokemon.id">
+      <img :src="pokemon.image_url" width="50" />
+      {{ pokemon.pokemon }}
+    </li>
+  </ul>
 </template>
+
+<style scoped>
+li {
+  display: flex;
+  align-items: center;
+}
+</style>
