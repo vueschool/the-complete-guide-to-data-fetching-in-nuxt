f6e68fafa8c85f9235ffe6c5a1e6786d06ec8083
Daniel Kelly <me@danielkelly.io>
2024-10-15 17:08:12 -0500

    2-usefetch-vs-useasyncdata-vs-fetch

---
+++ app/app.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts"></script>
+<template>
+  <NuxtLink to="/">Pokemon</NuxtLink> |
+  <NuxtLink to="/about">About</NuxtLink>
+  <NuxtPage />
+</template>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,9 +1,26 @@
 <script setup lang="ts">
+// useFetch vs $fetch vs useAsyncData
+
+function handleClick() {
+  $fetch("https://dummyapi.online/api/pokemon");
+}
+
 const { data: pokemons } = await useFetch(
   "https://dummyapi.online/api/pokemon"
 );
+
+async function myClientLibrary() {
+  return "my data";
+}
+
+const { data } = useAsyncData(async () => {
+  return await myClientLibrary();
+});
 </script>
 <template>
+  <h1>Pokemon</h1>
+  {{ data }}
+  <button @click="handleClick">Click Me</button>
   <ul>
     <li v-for="pokemon in pokemons" :key="pokemon.id">
       <img :src="pokemon.image_url" width="50" />
