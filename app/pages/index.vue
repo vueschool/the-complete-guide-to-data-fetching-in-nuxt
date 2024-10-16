0809187b95171092a488f35c78f5cf359a59e502
Daniel Kelly <me@danielkelly.io>
2024-10-16 16:46:26 -0500

    3-to-await-or-not-await-usefetch-blocking-vs-non-blocking

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,26 +1,10 @@
 <script setup lang="ts">
-// useFetch vs $fetch vs useAsyncData
-
-function handleClick() {
-  $fetch("/api/hello");
-}
-
-const { data: pokemons } = await useFetch(
-  "https://dummyapi.online/api/pokemon"
-);
-
-async function myClientLibrary() {
-  return "my data";
-}
-
-const { data } = useAsyncData(async () => {
-  return await myClientLibrary();
+const { data: pokemons } = await useFetch("/api/hello", {
+  lazy: true,
 });
 </script>
 <template>
   <h1>Pokemon</h1>
-  {{ data }}
-  <button @click="handleClick">Click Me</button>
   <ul>
     <li v-for="pokemon in pokemons" :key="pokemon.id">
       <img :src="pokemon.image_url" width="50" />
--- server/api/hello.ts
+++ server/api/hello.ts
@@ -1,3 +1,8 @@
 export default defineEventHandler(async (event) => {
+  await sleep(2000);
   return await $fetch("https://dummyapi.online/api/pokemon");
 });
+
+function sleep(milliseconds = 0) {
+  return new Promise((resolve) => setTimeout(resolve, milliseconds));
+}
