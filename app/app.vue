a3e6d36607f04e997f003a4cc4dea588e3c01aea
Daniel Kelly <me@danielkelly.io>
2024-10-17 15:05:51 -0500

    5-rerunning-fetch-requests-automatically-and-manually

--- app/app.vue
+++ app/app.vue
@@ -1,9 +1,22 @@
 <script setup lang="ts"></script>
 <template>
-  <div class="p-6">
+  <div>
     <NuxtLoadingIndicator />
-    <NuxtLink to="/">Pokemon</NuxtLink> |
-    <NuxtLink to="/about">About</NuxtLink>
-    <NuxtPage />
+    <UHorizontalNavigation
+      :links="[
+        {
+          label: 'Users',
+          to: '/',
+        },
+        {
+          label: 'About',
+          to: '/about',
+        },
+      ]"
+      class="border-b border-gray-200 dark:border-gray-800"
+    />
+    <div class="px-5">
+      <NuxtPage />
+    </div>
   </div>
 </template>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,21 +1,42 @@
 <script setup lang="ts">
-const { data: pokemons, status } = await useFetch("/api/hello", {
-  lazy: true,
-});
+const query = ref("");
+const committedQuery = ref("");
+
+const { data, status, execute} = await useFetch(
+  'https://dummyjson.com/users/search',
+  {
+    lazy: true,
+    query:{
+      q: committedQuery
+    }
+  }
+);
+
+const users = computed(() => data.value?.users);
 </script>
 <template>
-  <h1 class="text-3xl my-8">Pokemon</h1>
+  <h1 class="text-3xl my-8">Users</h1>
+  <UButton @click="execute">Refresh Data</UButton>
+  <form @submit.prevent="committedQuery = query" class="form">
+    <UInput type="text" v-model="query"/>
+    <UButton type="submit">Search</UButton>
+  </form>
   <ul>
     <template v-if="status === 'pending'">
-      <li v-for="n in 20" class="flex items-center space-x-4 h-[50px] pl-3">
+      <li v-for="n in 20">
         <USkeleton class="h-5 w-5" :ui="{ rounded: 'rounded-full' }" />
         <USkeleton class="h-2 w-[100px]" />
       </li>
     </template>
     <template v-else>
-      <li v-for="pokemon in pokemons" :key="pokemon.id">
-        <img :src="pokemon.image_url" width="50" />
-        {{ pokemon.pokemon }}
+      <li v-for="user in users" :key="user.id">
+        <img
+          :src="user.image"
+          :alt="user.username"
+          width="20"
+          class="rounded-full"
+        />
+        <span>{{ user.firstName }} {{ user.lastName }}</span>
       </li>
     </template>
   </ul>
@@ -23,7 +44,9 @@ const { data: pokemons, status } = await useFetch("/api/hello", {
 
 <style scoped>
 li {
-  display: flex;
-  align-items: center;
+  @apply flex items-center space-x-4 h-[50px] pl-3;
+}
+.form {
+  @apply flex items-center w-96 gap-2 mb-5;
 }
 </style>
