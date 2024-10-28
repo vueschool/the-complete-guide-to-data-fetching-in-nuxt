d81d222c73a16306ac1a4693d2092957a63340ef
Daniel Kelly <me@danielkelly.io>
2024-10-28 14:39:25 -0500

    8-1-caching-responses-with-usefetch-and-useasyncdata-in-nuxt

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,10 +1,18 @@
 <script setup lang="ts">
-
-const { data } = await useAsyncData(Math.random().toString(), () =>
-  $fetch(`/api/randomUser?randomNum=${Math.random()}`),
-  {
-    lazy: true,
-  })
+const { data } = await useFetch('/api/randomUser', {
+  transform(data) {
+    return {
+      ...data,
+      expiresAt: Date.now() + 10_000
+    }
+  },
+  getCachedData(key, nuxtApp) {
+    const data = nuxtApp.payload.data[key] || nuxtApp.static.data[key]
+    if (Date.now() >= data?.expiresAt) return undefined;
+    return data;
+  },
+  lazy: true,
+});
 </script>
 <template>
   <h1 class="text-3xl my-8">Users</h1>
