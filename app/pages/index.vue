43b0b24ffb76ad1c8d8c81c1dc2cc71a537fbc26
Daniel Kelly <me@danielkelly.io>
2024-10-29 14:00:17 -0500

    11-how-data-is-serialized-from-server-to-client-in-nuxt

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,17 +1,11 @@
 <script setup lang="ts">
-const { data, execute } = await useFetch('/api/randomUser', {
-  immediate: false,
-  dedupe: "defer",
-  default: () => {
-    return {
-      firstName: "John",
-      lastName: "Doe"
-    }
-  }
-});
+const { data } = await useFetch('/api/currentDate')
+
+onMounted(() => {
+  alert(data.value?.getHours())
+})
 </script>
 <template>
   <h1 class="text-3xl my-8">Users</h1>
-  <button @click="execute()">Click me</button>
   <pre>{{ data }}</pre>
 </template>
